f6015ab744bb8e21b45f7ff175a51d78e040e5bd
nadevvs <idnadeev@gmail.com>
2026-05-02 01:00:01 +0200

    updated keygen.py to do real private key generation using external 'secrets' module and computation of public key

--- core/keygen.py
+++ core/keygen.py
@@ -1,8 +1,19 @@
-def generate_keypair(private_key=None):
-    # placeholder
+import secrets
+from core.curve import get_p256_curve, scalar_mult
+
+
+def generate_private_key(n: int) -> int:
+    return secrets.randbelow(n - 1) + 1
+
+
+def generate_keypair(private_key: int | None = None):
+    curve = get_p256_curve()
 
     if private_key is None:
-        private_key = 12345
+        private_key = generate_private_key(curve.n)
+    else:
+        if not (1 <= private_key <= curve.n - 1):
+            raise ValueError("Private key must be in the range [1, n - 1].")
 
-    public_key = (11111, 22222)
+    public_key = scalar_mult(private_key, curve.g, curve)
     return private_key, public_key
